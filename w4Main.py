a4d5cc0dde279c7cd82cca8ef4c1db7c5be99ca2
bckang92 <bckang92@naver.com>
2016-03-28 13:48:06 +0900

    w4 Homework

---
+++ w4Main.py
@@ -0,0 +1,18 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def makeSwirlSquare(size, bigger, sides, angle):
+    t1.home()
+    t1.clear()
+    for i in range(0,sides):
+        if i%2:
+            size=size+bigger
+        else:
+            size
+        t1.fd(size)
+        t1.right(angle)
+        
+makeSwirlSquare(10, 10, 10, 90)
+
+wn.exitonclick()
