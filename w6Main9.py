e212a6544bb2c3a86f514bbdc2fdd4346636b39a
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-04-06 15:06:51 +0900

    homework

---
+++ w6Main9.py
@@ -0,0 +1,11 @@
+﻿def sum3_5(begin, end):
+    sum=0
+    for i in range(begin, end):
+        if (i % 3 == 0 or i % 5 == 0):
+            sum = i+sum
+    print sum
+    return sum
+
+sum3_5(0, 1001)        
+
+raw_input()
