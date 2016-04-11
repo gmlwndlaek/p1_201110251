c7db52bfbeff7e28d0718db00c7f2cde8551935d
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-04-11 23:34:09 +0900

    homework

---
+++ w7main1.py
@@ -0,0 +1,17 @@
+﻿x=list()
+for i in range(0,1001):
+    if(i%4==0 and i%5!=0):
+        print i
+        x.append(i)
+
+def sumList(list):
+    x=list
+    sum=0
+
+    for i in range(0,len(x)):
+        sum=sum+x[i]
+
+    return sum
+print sumList(x)
+
+raw_input()
