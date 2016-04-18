551984c4228618caeabcd6ce6b7b7f254df3296d
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-04-19 00:01:09 +0900

    과제 최종입니다

---
+++ w7main(1).py
@@ -0,0 +1,33 @@
+﻿import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle() 
+ 
+def drawSquareAtSave(size,pos): 
+    tracks=list() 
+    t1.penup() 
+    t1.setpos(pos) 
+    for i in range(0,4): 
+        t1.fd(size) 
+        t1.left(90) 
+        tracks.append(t1.pos()) 
+    return tracks 
+
+def lab7(): 
+    size=100 
+    pos=t1.pos() 
+    mytracks=drawSquareAtSave(size,pos) 
+    drawSquareFront_list(mytracks) 
+    print mytracks 
+
+def main(): 
+    lab7() 
+    
+def drawSquareFront_list(tracks): 
+    t1.penup() 
+    t1.goto(tracks[3]) 
+    t1.pendown() 
+    for i in range(4): 
+        t1.goto(tracks[i])
+        
+if __name__=="__main__":
+    main() 
