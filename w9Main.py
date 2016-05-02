3010fbaa70fb96f4c3b6c5e6d1ada21de0c853bb
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-05-02 13:12:20 +0900

    Added files via upload

---
+++ w9Main.py
@@ -0,0 +1,19 @@
+﻿def charCount(word):
+    w= word
+    d=dict()
+    for c in w:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+    print d
+    
+def lab9():
+    charCount('sangmyung')
+
+def main():
+    lab9()
+    
+if __name__=="__main__":
+    main()
+
