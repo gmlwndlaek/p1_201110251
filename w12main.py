a89bd18e6795be0bdb4053757b3ab841c0776b81
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-05-23 22:57:59 +0900

    homework

---
+++ w12main.py
@@ -0,0 +1,8 @@
+﻿try:
+    myfile=open(myfilename,'r')
+    for line in myfile:
+        if line.find('Python')>=0:
+            print line
+    myfile.close()
+except IOError as e:
+    print e
---
+++ w12main2.py
@@ -0,0 +1,16 @@
+﻿myfile=open('output.txt', 'w')
+line1='first line\n'
+myfile.write(line1)
+line2='\tsecond line\n'
+myfile.write(line2)
+line3='third'
+myfile.write(line3)
+myfile.close()
+
+myfile=open('output.txt', 'r')
+for i in myfile:
+    if i.find('line')>=0:
+        print i.upper()
+    else:
+        print i
+myfile.close()
