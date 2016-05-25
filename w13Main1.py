2fe965b7b6eabb96cfc1584b67c20b50b02aa055
Maeng Hee Ju <gmlwndlaek@naver.com>
2016-05-26 01:06:57 +0900

    homework

---
+++ w13Main1.py
@@ -0,0 +1,16 @@
+﻿import time
+editor='mhj'
+timeEdited=time.strftime(' %Y-%m-%d''%H:%M:%S')
+fin=open('output.txt', 'r')
+fout=open('outputUpper.txt','w')
+for line in fin:
+    words=line.split()
+    for word in words:
+        if word=='line':
+            word = word.upper()
+            print "[{0} edited {1}] {2}".format(editor,timeEdited,word)
+        else:
+            print word
+
+fin.close()
+fout.close()
---
+++ w13Main2.py
@@ -0,0 +1,9 @@
+﻿data=[1,2,3,4,5,6,7,8,9,10]
+fout=open('outputNumber.txt', 'w')
+for i in data:
+    toPrint="{0}\t".format(i)
+    if (i%2==0):
+        toPrint="{0}\t\n".format(i)
+    fout.write(toPrint)
+fout.close()
+%load outputNumber.txt
